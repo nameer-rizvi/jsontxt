726cd3c0be126a749a7a57f505442df471b32a75
nameer-rizvi <rizvinameer@gmail.com>
2024-05-24 12:27:55 -0400

    package update

--- src/filesize.js
+++ src/filesize.js
@@ -7,16 +7,16 @@ function filesize(option) {
       const stat = fs.statSync(filepath(option));
 
       const size = {
-        bytes: stat.size,
-        kilobytes: stat.size / 1000,
-        megabytes: stat.size / 1000000,
-        gigabytes: stat.size / 1000000000,
-        terabytes: stat.size / 1000000000000,
-        b: stat.size.toLocaleString() + " bytes",
-        kb: (stat.size / 1000).toLocaleString() + " kb",
-        mb: (stat.size / 1000000).toLocaleString() + " mb",
-        gb: (stat.size / 1000000000).toLocaleString() + " gb",
-        tb: (stat.size / 1000000000000).toLocaleString() + " tb",
+        b: stat.size,
+        kb: stat.size / 1000,
+        mb: stat.size / 1000000,
+        gb: stat.size / 1000000000,
+        tb: stat.size / 1000000000000,
+        bytes: stat.size.toLocaleString() + " bytes",
+        kilobytes: (stat.size / 1000).toLocaleString() + " kb",
+        megabytes: (stat.size / 1000000).toLocaleString() + " mb",
+        gigabytes: (stat.size / 1000000000).toLocaleString() + " gb",
+        terabytes: (stat.size / 1000000000000).toLocaleString() + " tb",
       };
 
       return size;
