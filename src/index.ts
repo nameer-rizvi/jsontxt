681ab45756cc4d8845abea49297af8443728af6b
nameer-rizvi <rizvinameer@gmail.com>
2026-04-20 13:18:34 -0400

    Fix exports in index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { _delete } from "./delete.js";
-import * as filepath from "./filepath.js";
-import * as filesize from "./filesize.js";
-import * as read from "./read.js";
-import * as write from "./write.js";
+import { filepath, type FilePathOptions } from "./filepath.js";
+import { filesize, UNITS } from "./filesize.js";
+import { read } from "./read.js";
+import { write } from "./write.js";
 
-export default { delete: _delete, ...filepath, ...filesize, ...read, ...write };
+export default { delete: _delete, filepath, filesize, UNITS, read, write };
+export type { FilePathOptions };
